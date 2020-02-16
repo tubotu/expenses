bede9a28542d7a9d685fc3f6c501616d98a39484
TaichiTakeda <ticadeleaper.edp@gmail.com>
2020-02-17 03:11:52 +0900

    グラフの表示（セッション利用のための実験）

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
-app_name = 'app'
+
+app_name = "app"
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('signup/', views.signup, name='signup'),
-    path('login/',
-        auth_views.LoginView.as_view(template_name='app/login.html'),
-        name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-] 
+    path("", views.index, name="index"),
+    path("signup/", views.signup, name="signup"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="app/login.html"),
+        name="login",
+    ),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("graph_outgo/", views.graph_outgo, name="graph_outgo"),
+    path("popup_table/<int:point_id>/", views.popup_table, name="popup_table"),
+]
+
--- app/views.py
+++ app/views.py
@@ -2,20 +2,77 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from .forms import CustomUserCreationForm
 
+from bokeh.models import ColumnDataSource, OpenURL, TapTool, CustomJS
+from bokeh.plotting import figure
+from bokeh.resources import CDN
+from bokeh.embed import components
+
+
 def index(request):
-    return render(request, 'app/index.html')
+    return render(request, "app/index.html")
+
 
 def signup(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             new_user = form.save()
-            input_user_name = form.cleaned_data['user_name']
-            input_password = form.cleaned_data['password1']
+            input_user_name = form.cleaned_data["user_name"]
+            input_password = form.cleaned_data["password1"]
             new_user = authenticate(email=input_email, password=input_password)
         if new_user is not None:
             login(request, new_user)
-            return redirect('app:index')
+            return redirect("app:index")
     else:
         form = CustomUserCreationForm()
-    return render(request, 'app/signup.html', {'form': form})
+    return render(request, "app/signup.html", {"form": form})
+
+
+def graph_outgo(request):
+
+    x = [1, 2, 3, 4, 5]
+    y = [200, 500, 800, 200, 700]
+    point_id = [0, 1, 2, 3, 4]
+
+    for id_ in point_id:
+        if "price" in request.session:
+            request.session["price"][str(id_)] = y[id_]
+        else:
+            request.session["price"] = {str(id_): y[id_]}
+
+    price = request.session.get("price", {})
+    print(price)
+
+    p = figure(plot_width=400, plot_height=400, tools="tap", title="Click the Dots")
+
+    source = ColumnDataSource(data=dict(x=x, y=y, point_id=point_id,))
+    p.line("x", "y", source=source)
+    p.circle("x", "y", size=20, source=source)
+
+    # use the "color" column of the CDS to complete the URL
+    # e.g. if the glyph at index 10 is selected, then @color
+    # will be replaced with source.data['color'][10]
+    # url = "http://www.colors.commutercreative.com/@color/"
+
+    url = "http://127.0.0.1:8000/popup_table/@point_id"
+    taptool = p.select(type=TapTool)
+
+    taptool.callback = OpenURL(url=url, same_tab=True)
+
+    cdn_js = CDN.js_files[0]
+    cdn_css = CDN.css_files
+    script, div = components(p)
+
+    return render(
+        request,
+        "app/graph_outgo.html",
+        {"cdn_js": cdn_js, "cdn_css": cdn_css, "script": script, "div": div},
+    )
+
+
+def popup_table(request, point_id):
+
+    price = request.session["price"]
+    print(price)
+
+    return render(request, "app/popup_table.html", {"price": price[str(point_id)]})
