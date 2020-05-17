803875a1c795f77d46eba80a46118ab084b8a9c6
tubotu <rin081225@ezweb.ne.jp>
2020-05-17 23:57:19 +0900

    modelsの更新

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 class BigCategory(models.Model):
     big_category = models.CharField(max_length=20, default='その他', unique=True)
-    #user = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     def __str__(self):
         return self.big_category
 
@@ -23,11 +23,10 @@ class Item(models.Model):
     item = models.CharField(max_length=30)     
     description = models.TextField(blank=True)
     small_category = models.ForeignKey(SmallCategory
-        , on_delete=models.PROTECT
+        , on_delete=models.CASCADE # userを消したら消えるようにしたい,small_categoryだけ消すときは移植作業が必要
     )
     price = models.PositiveIntegerField(default=0)
     paid_at = models.DateField(default=timezone.now)
-    user = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
 
     def __str__(self):         
         return self.item
